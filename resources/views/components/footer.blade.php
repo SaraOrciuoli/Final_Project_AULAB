4a173c446ce50dbc1f11f17ecaa47bcfde60c930
username / AngeloDiNome <angelofulldev@gmail.com>
2023-06-12 20:27:24 +0200

    modificato button footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="container-fluid bg-sec">
         <div class="row height-footer">
-            <div class="col-12 col-md-8 d-flex flex-column align-items-center justify-content-center">
+            <div class="col-12 col-md-8 d-flex flex-column align-items-center justify-content-center bg">
                 <a href="{{route('homepage')}}">
                     <img class="logo mb-3" src="/media/logo-b.png" alt="Logo">
                 </a>
                 <p class="lead">Vuoi lavorare con noi?</p>
                 <p>Registrati e clicca qui!</p>
                 <a href="{{route('become_revisor')}}" class="learn-more button">
-                    <span class="circle" aria-hidden="true">
+                    <span class="circle circle-foot" aria-hidden="true">
                         <span class="icon arrow"></span>
                     </span>
-                    <span class="button-text">Diventa revisore</span>
+                    <span class="button-footer">Diventa revisore</span>
                 </a>
             </div>
             <div class="col-12 col-md-4 d-flex align-items-center">
