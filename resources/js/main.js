ab3ae296dc5103d24066170ba918bbd2d8fd00b0
Alessio-Zamuner <alessio.zamuner7@gmail.com>
2023-06-12 14:57:44 +0200

    riordinato fogli css

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,11 +2,16 @@
 
 import Swiper from "swiper/bundle";
 const swiper = new Swiper(".mySwiper", {
+    loop: true,
     spaceBetween: 30,
     effect: "fade",
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
     },
     pagination: {
         el: ".swiper-pagination",
@@ -15,24 +20,21 @@ const swiper = new Swiper(".mySwiper", {
 });
 
 // navbar
-let navbar = document.querySelector('.navbar');
-let navLink = document.querySelector('.nav-link');
-let navContainer = document.querySelector('.navContainer');
-window.addEventListener('scroll', () => {
+let navbar = document.querySelector(".navbar");
+let navLink = document.querySelector(".nav-link");
+let navContainer = document.querySelector(".navContainer");
+window.addEventListener("scroll", () => {
     let scrolled = window.scrollY;
 
     if (scrolled > 20) {
-        navbar.classList.add('bg-acc');
-        navbar.classList.remove('bg-main');
-        navbar.classList.add('navContainer');
-        navbar.classList.remove('navbar');
+        navbar.classList.add("bg-acc");
+        navbar.classList.remove("bg-main");
+        navbar.classList.add("navContainer");
+        navbar.classList.remove("navbar");
+    } else {
+        navbar.classList.add("bg-main");
+        navbar.classList.remove("bg-acc");
+        navbar.classList.remove("navContainer");
+        navbar.classList.add("navbar");
     }
-    else {
-        navbar.classList.add('bg-main');
-        navbar.classList.remove('bg-acc');
-        navbar.classList.remove('navContainer');
-        navbar.classList.add('navbar');
-    }
-
-
-})
+});
