54fa916474607fca5e7431b869e439ab7e693d1b
Alessio-Zamuner <alessio.zamuner7@gmail.com>
2023-06-12 11:09:38 +0200

    creata component card_announcement e aggiunto foglio di stile

---
+++ resources/views/components/card_announcement.blade.php
@@ -0,0 +1,16 @@
+<div class="col-12 col-md-3 m-2 bg-light my-5">
+    <div class="card">
+        <img src="https://picsum.photos/100" class="card-img-top" alt="Image Announcement">
+        <div class="card-body">
+            <h5 class="card-title">{{ $announcement->title }}</h5>
+            <p class="card-title">{{ $announcement->description }}</p>
+            <p class="card-subtitle">{{ $announcement->price }}€</p>
+            <a href="{{route('category_show', ['category' => $announcement->category])}}" class="btn btn-outline-dark my-2">Categoria:
+                {{ $announcement->category->name }}</a>
+            <p class="card-footer">Pubblicato il: {{ $announcement->created_at->format('d/m/Y') }}</p>
+            <div class="d-flex justify-content-around m-2">
+                <a href="{{route('announcement_show', compact('announcement'))}}" class="btn btn-primary">Dettagli</a>
+            </div>
+        </div>
+    </div>
+</div>
