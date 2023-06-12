d53c7e00c789c729099c0932331ec9cfb3f9b37a
Mattia Falchi <mattia.falchi92@outlook.it>
2023-06-12 13:23:52 +0200

    modificato testo footer(hover e fon family

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,42 +2,42 @@
     <div class="container">
         <div class="row row-pb-md">
             <div class="col-md-4 ">
-                <h3>Shop.</h3>
+                <h3 class="title-footer">Shop.</h3>
                 <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-                <ul class="links">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Help</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Terms</a></li>
-                    <li><a href="#">Meetups</a></li>
+            <div class="col-md-2 col-sm-4 col-xs-6 ">
+                <ul>
+                    <li><a class="links" href="#">About</a></li>
+                    <li><a class="links" href="#">Help</a></li>
+                    <li><a class="links" href="#">Contact</a></li>
+                    <li><a class="links" href="#">Terms</a></li>
+                    <li><a class="links" href="#">Meetups</a></li>
                 </ul>
             </div>
 
-            <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-                <ul class="links">
-                    <li><a href="#">Shop</a></li>
-                    <li><a href="#">Privacy</a></li>
-                    <li><a href="#">Testimonials</a></li>
-                    <li><a href="#">Handbook</a></li>
-                    <li><a href="#">Held Desk</a></li>
+            <div class="col-md-2 col-sm-4 col-xs-6 text-footer">
+                <ul>
+                    <li><a class="links" href="#">Shop</a></li>
+                    <li><a class="links" href="#">Privacy</a></li>
+                    <li><a class="links" href="#">Testimonials</a></li>
+                    <li><a class="links" href="#">Handbook</a></li>
+                    <li><a class="links" href="#">Held Desk</a></li>
                 </ul>
             </div>
 
-            <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-                <ul class="links">
-                    <li><a href="#">Find Designers</a></li>
-                    <li><a href="#">Find Developers</a></li>
-                    <li><a href="#">Teams</a></li>
-                    <li><a href="#">Advertise</a></li>
-                    <li><a href="#">API</a></li>
+            <div class="col-md-2 col-sm-4 col-xs-6 text-footer">
+                <ul >
+                    <li><a class="links" href="#">Find Designers</a></li>
+                    <li><a class="links" href="#">Find Developers</a></li>
+                    <li><a class="links" href="#">Teams</a></li>
+                    <li><a class="links" href="#">Advertise</a></li>
+                    <li><a class="links" href="#">API</a></li>
                 </ul>
             </div>
         </div>
 
         <div class="row copyright ">
-            <div class="col-md-12 text-center ">
+            <div class="col-md-12 text-center text-footer">
                 <p>
                     <p class="block text-center">© 2018 Free HTML5. All Rights Reserved.</p> 
                     <p class="block">Designed by <a class="free" href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://blog.gessato.com/" target="_blank">Gessato</a> &amp; <a class="free" href="http://unsplash.co/" target="_blank">Unsplash</a></p>
