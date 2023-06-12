f34e46a781d4469308cd992826eadcb5a3cd0dfb
Mattia Falchi <mattia.falchi92@outlook.it>
2023-06-12 12:53:37 +0200

    modificato footer e iniziato a personalizzare

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,60 @@
-<div class="container-fluid mt-4 p-5 bg-dark text-light">
+<footer >
+    <div class="container">
+        <div class="row row-pb-md">
+            <div class="col-md-4 ">
+                <h3>Shop.</h3>
+                <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                <ul class="links">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Meetups</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                <ul class="links">
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                    <li><a href="#">Handbook</a></li>
+                    <li><a href="#">Held Desk</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                <ul class="links">
+                    <li><a href="#">Find Designers</a></li>
+                    <li><a href="#">Find Developers</a></li>
+                    <li><a href="#">Teams</a></li>
+                    <li><a href="#">Advertise</a></li>
+                    <li><a href="#">API</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row copyright ">
+            <div class="col-md-12 text-center ">
+                <p>
+                    <p class="block text-center">© 2018 Free HTML5. All Rights Reserved.</p> 
+                    <p class="block">Designed by <a class="free" href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://blog.gessato.com/" target="_blank">Gessato</a> &amp; <a class="free" href="http://unsplash.co/" target="_blank">Unsplash</a></p>
+                </p>
+                <ul class="d-flex list-unstyled ">
+                        <li><a href="#"><i class="icons fa-brands fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="icons fa-brands fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="icons fa-brands fa-linkedin-in"></i></a></li>
+                        <li><a href="#"><i class="icons fa-brands fa-dribbble"></i></a></li>
+                    </ul>
+                
+            </div>
+        </div>
+
+    </div>
+</footer>
+{{-- <div class="container-fluid mt-4 p-5 bg-dark text-light">
     <div class="row">
         <div class="col-12 text-center">
             <p>Presto.it</p>
@@ -7,4 +63,4 @@
             <a href="{{route('become_revisor')}}" class="btn btn-warning text-light shadow my-3">Diventa revisore</a>
         </div>
     </div>
-</div>
+</div> --}}
