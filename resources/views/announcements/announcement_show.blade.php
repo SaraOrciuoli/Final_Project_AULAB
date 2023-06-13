7109f993af7587da041b8d98c317e0f352cb5bef
Alessio-Zamuner <alessio.zamuner7@gmail.com>
2023-06-13 21:31:26 +0200

    modificate viste revisor, component card e announcement show importando dinamicamente le immagini

--- resources/views/announcements/announcement_show.blade.php
+++ resources/views/announcements/announcement_show.blade.php
@@ -12,20 +12,31 @@
         <div class="row">
             <div class="col-12">
                 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
-                                alt="img">
+                    @if ($announcement->images)
+                        <div class="carousel-inner">
+                            @foreach ($announcement->images as $image)
+                                <div class="carousel-item @if ($loop->first) active @endif">
+                                    <img src="{{ Storage::url($image->path) }}" class="img-fluid p-3 rounded"
+                                        alt="img">
+                                </div>
+                            @endforeach
                         </div>
-                        <div class="carousel-item">
-                            <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
-                                alt="img">
+                    @else
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
+                                    alt="img">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
+                                    alt="img">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
+                                    alt="img">
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
-                                alt="img">
-                        </div>
-                    </div>
+                    @endif
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
                         data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -40,9 +51,10 @@
                 <h5 class="card-title">{{ $announcement->title }}</h5>
                 <p class="card-title">{{ $announcement->description }}</p>
                 <p class="card-subtitle">{{ $announcement->price }}€</p>
-                <a href="{{route('category_show', ['category' => $announcement->category])}}" class="btn btn-card-announcement my-3">Categoria:
+                <a href="{{ route('category_show', ['category' => $announcement->category]) }}"
+                    class="btn btn-card-announcement my-3">Categoria:
                     {{ $announcement->category->name }}</a>
-                
+
                 <p class="card-footer">Pubblicato il: {{ $announcement->created_at->format('d/m/Y') }} -
                     Autore: {{ $announcement->user->name ?? '' }}</p>
             </div>
--- resources/views/revisor/revisor_index.blade.php
+++ resources/views/revisor/revisor_index.blade.php
@@ -4,37 +4,48 @@
         <div class="row">
             <div class="col-12 text-light p-5">
                 <h1 class="display-2">
-                    {{$announcement_to_check ? 'Annuncio da rivisionare' : 'Non ci sono annunci da revisionare'}}
+                    {{ $announcement_to_check ? 'Annuncio da rivisionare' : 'Non ci sono annunci da revisionare' }}
                 </h1>
             </div>
         </div>
     </div>
 
-    @if($announcement_to_check)
+    @if ($announcement_to_check)
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div id="showCarousel" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
-                                    alt="img">
+                        @if ($announcement_to_check->images)
+                            <div class="carousel-inner">
+                                @foreach ($announcement_to_check->images as $image)
+                                    <div class="carousel-item @if($loop->first)active @endif">
+                                        <img src="{{ Storage::url($image->path) }}" class="img-fluid p-3 rounded"
+                                            alt="img">
+                                    </div>
+                                @endforeach
                             </div>
-                            <div class="carousel-item">
-                                <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
-                                    alt="img">
+                        @else
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
+                                        alt="img">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
+                                        alt="img">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
+                                        alt="img">
+                                </div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="https://picsum.photos/id/27/1200/200" class="img-fluid p-3 rounded"
-                                    alt="img">
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        @endif
+                        <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
                             data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
                             data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
@@ -49,14 +60,17 @@
 
             <div class="row ">
                 <div class="col-12 col-md-6 d-flex">
-                    <form method="POST" action="{{route('revisor_accept_announcement', ['announcement' => $announcement_to_check])}}">
+                    <form method="POST"
+                        action="{{ route('revisor_accept_announcement', ['announcement' => $announcement_to_check]) }}">
                         @csrf
                         @method('patch')
 
                         <button type="submit" class="btn btn-success shadow">Accetta Annuncio</button>
                     </form>
 
-                    <form method="POST" action="{{route('revisor_reject_announcement', ['announcement' => $announcement_to_check])}}" class="mx-4">
+                    <form method="POST"
+                        action="{{ route('revisor_reject_announcement', ['announcement' => $announcement_to_check]) }}"
+                        class="mx-4">
                         @csrf
                         @method('patch')
 
