8af5cbcb6b5fcfa555501af605942e74538a2f9b
Mattia Falchi <mattia.falchi92@outlook.it>
2023-06-15 09:58:03 +0200

    sistemata traduzione  registrazion

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,33 +8,33 @@
 
                 <div class="col-12 col-md-6 float-end d-flex align-items-center justify-content-center position-relative">
                     <div class="input-box">
-                        <h4 class="title-form text-lightDark">Crea account</h4>
+                        <h4 class="title-form text-lightDark">{{__('ui.crea account')}}</h4>
                         <form method="POST" action="{{route('register')}}">
                             @csrf
         
                             <div class="box-form my-4">
                                 <input type="text" class="form-control form-control-custom input-custom input-form" id="name" name="name">
-                                <label for="name" class="form-label">Nome Utente</label>
+                                <label for="name" class="form-label">{{__('ui.nome utente')}}</label>
                             </div>
         
                             <div class="box-form my-4">
                                 <input type="email" class="form-control form-control-custom input-custom" id="userEmail" name="email" aria-describedby="emailHelp">
-                                <label for="userEmail" class="form-label">Email</label>
+                                <label for="userEmail" class="form-label">{{__('ui.email')}}</label>
                             </div>
         
                             <div class="box-form my-4">
                                 <input type="password" class="form-control form-control-custom input-custom" id="userPassword" name="password">
-                                <label for="userPassword" class="form-label">Password</label>
+                                <label for="userPassword" class="form-label">{{__('ui.password')}}</label>
                             </div>
         
                             <div class="box-form my-4">
                                 <input type="password" class="form-control form-control-custom input-custom" id="passwordConfirmation" name="password_confirmation">
-                                <label for="passwordConfirmation" class="form-label">Conferma Password</label>
+                                <label for="passwordConfirmation" class="form-label">{{__('ui.conferma password')}}</label>
                             </div>
         
                             <div>
-                                <button type="submit" class="btn btn-card-announcement my-1">Registrati!</button>
-                                <a href="{{route('login')}}" class="btn-register">Hai già un account? Accedi!</a>
+                                <button type="submit" class="btn btn-card-announcement my-1">{{__('ui.registrati')}}!</button>
+                                <a href="{{route('login')}}" class="btn-register">{{__('ui.Hai già un account? Accedi!')}}</a>
                             </div>
                         </form>
                     </div>
