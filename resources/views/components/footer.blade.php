fe27230f6692bd616606ac52e7d8361c071edf04
username / AngeloDiNome <angelofulldev@gmail.com>
2023-06-15 16:18:50 +0200

    reso responsive footer e pagina register

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,52 +1,70 @@
-<footer class="container-fluid bg-sec">
-        <div class="row justify-content-center p-5">
-            <div class="col-12 col-md-4">
-                <a href="{{route('homepage')}}">
+<footer class="container-fluid bg-sec p-0 p-md-5 h-100">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-4 p-4 p-md-0">
+                <a href="{{route('homepage')}}" class="d-flex justify-content-center justify-content-md-start">
                     <img class="logo" src="/media/logo-b.png" alt="Logo">
                 </a>
-                <p class="text-testimonial text-start mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quos debitis reiciendis aperiam quis molestiae, reprehenderit tenetur? Deserunt accusamus dicta blanditiis.</p>
+                <p class="text-testimonial text-center text-md-start mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quos debitis reiciendis aperiam quis molestiae, reprehenderit tenetur? Deserunt accusamus dicta blanditiis.</p>
             </div>
-
-            <div class="col-12 col-md-4 d-flex align-items-center justify-content-evenly mt-4">
+            <div class="col-12 col-md-4 d-md-flex align-items-center justify-content-evenly mt-4 p-0 p-md-3">
                 <ul class="text-lightDark">
                     <h6 class="text-lightDark">{{__('ui.servizi')}}:</h6>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                    <div class="d-flex d-md-block">
+                        <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                        <li><a href="#" class="text-lightDark a-footer mx-3 mx-md-0">Lorem ipsum</a></li> 
+                        <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                    </div>
                 </ul>
                 <ul class="text-lightDark">
                     <h6 class="text-lightDark">{{__('ui.rimborsi')}}:</h6>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                    <div class="d-flex d-md-block">
+                        <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                        <li><a href="#" class="text-lightDark a-footer mx-3 mx-md-0">Lorem ipsum</a></li> 
+                        <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                    </div>
                 </ul>
                 <ul class="text-lightDark">
                     <h6 class="text-lightDark">{{__('ui.assistenza')}}:</h6>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
-                    <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                    <div class="d-flex d-md-block">
+                        <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                        <li><a href="#" class="text-lightDark a-footer mx-3 mx-md-0">Lorem ipsum</a></li> 
+                        <li><a href="#" class="text-lightDark a-footer">Lorem ipsum</a></li>
+                    </div>
+                </ul>
+                <hr class="d-block d-md-none hr-footer mt-4">
+                <ul class="d-flex justify-content-center mt-4 p-0 d-block d-md-none">
+                    <li>
+                        <a href="#"><i class="fa-brands fa-twitter fa-xl icon-footer mx-4 mx-md-5"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-brands fa-facebook fa-xl icon-footer mx-4 mx-md-5"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-brands fa-linkedin fa-xl icon-footer mx-4 mx-md-5"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-brands fa-whatsapp fa-xl icon-footer mx-4 mx-md-5"></i></a>
+                    </li>
                 </ul>
             </div>
         </div>
-
+        <hr class="d-block d-md-none hr-footer">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-8 text-center ">
+            <div class="col-12 col-md-8 text-center mt-md-4 my-4 my-md-0">
                 <small class="text-testimonial fst-italic fw-light">© 2023 lorem ipsum. Dolor Sit Amet Const.</small>
                 <p class="text-testimonial">{{__('ui.progettato da')}} <a href="https://aulab.it/" target="_blank" class="text-acc fw-bold">aulab.it</a> Lorem ipsum dolor</p>
-                <ul class="d-flex justify-content-center mt-4 p-0">
+                <ul class="d-flex justify-content-center mt-4 p-0 d-none d-md-block">
                     <li>
-                        <a href="#"><i class="fa-brands fa-twitter fa-xl icon-footer mx-5"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter fa-xl icon-footer mx-4 mx-md-5"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa-brands fa-facebook fa-xl icon-footer mx-5"></i></a>
+                        <a href="#"><i class="fa-brands fa-facebook fa-xl icon-footer mx-4 mx-md-5"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa-brands fa-linkedin fa-xl icon-footer mx-5"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin fa-xl icon-footer mx-4 mx-md-5"></i></a>
                     </li>
-                    <li><a href="#"><i class="fa-brands fa-whatsapp fa-xl icon-footer mx-5"></i></a>
+                    <li>
+                        <a href="#"><i class="fa-brands fa-whatsapp fa-xl icon-footer mx-4 mx-md-5"></i></a>
                     </li>
                 </ul>
             </div>
