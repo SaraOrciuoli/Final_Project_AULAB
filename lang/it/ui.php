23a770ee1e5403f2e14522bfbe4a077388bf334c
username / AngeloDiNome <angelofulldev@gmail.com>
2023-06-16 23:01:25 +0200

    personalizzato pagina contatti

--- lang/it/ui.php
+++ lang/it/ui.php
@@ -72,5 +72,4 @@ return [
     'invia' => 'Invia',
     'contattaci' => 'Contattaci'
 
-
 ];
--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,39 +1,43 @@
 <x-layout>
-    <div class="wrapper">
-        <div class="container-fluid d-flex justify-content-center align-items-center min-vh-100 ">
-            <div class="row row-form animation-fade">
-                <div class="col-12 col-md-6">
-                    <h4 class="title-form text-lightDark">{{__('ui.contattaci')}}</h4>
-                    <form method="POST" action="{{ route('send') }}">
-                        @csrf
+    <div class="bg-acc">
+        <div class="container d-flex justify-content-center align-items-center min-vh-100 p-9">
+            <div class="row row-form animation-fade h-100">
 
-                        <div class="box-form my-4">
-                            <input type="text" class="form-control form-control-custom input-custom input-form"
-                                id="name" name="user">
-                            <label for="name" class="form-label">{{ __('ui.nome utente') }}</label>
-                        </div>
+                <div class="col-12 col-md-6 img-side"></div>
 
-                        <div class="box-form my-4">
-                            <input type="email" class="form-control form-control-custom input-custom" id="userEmail"
-                                name="email" aria-describedby="emailHelp">
-                            <label for="userEmail" class="form-label">{{ __('ui.email') }}</label>
-                        </div>
-
-
-                        <div class="box-form my-4">
-                            <textarea class="form-control form-control-custom input-custom" name="body" id="" cols="30"
-                                rows="10"></textarea>
-                            <label for="userEmail" class="form-label">{{ __('ui.scrivi un messaggio') }}</label>
-                        </div>
-                        <div>
-                            <button type="submit" class="btn btn-card-announcement my-1">{{ __('ui.invia') }}</button>
-                        </div>
-
-                    </form>
+                <div class="col-12 col-md-6 float-end d-flex align-items-center justify-content-center position-relative p-md-5">
+                    <div class="input-box">
+                        <h4 class="title-form text-lightDark mt-5 mt-md-0">{{__('ui.contattaci')}}</h4>
+                        <form method="POST" action="{{ route('send') }}">
+                            @csrf
+                        
+                            <div class="box-form my-4">
+                                <input type="text" class="form-control form-control-custom input-custom input-form"
+                                    id="name" name="user">
+                                <label for="name" class="form-label">{{ __('ui.nome utente') }}</label>
+                            </div>
+                        
+                            <div class="box-form my-4">
+                                <input type="email" class="form-control form-control-custom input-custom" id="userEmail"
+                                    name="email" aria-describedby="emailHelp">
+                                <label for="userEmail" class="form-label">{{ __('ui.email') }}</label>
+                            </div>
+                        
+                        
+                            <div class="box-form my-4">
+                                <textarea class="form-control form-control-custom input-custom" name="body" id="" cols="30"
+                                    rows="10"></textarea>
+                                <label for="userEmail" class="form-label">{{ __('ui.scrivi un messaggio') }}</label>
+                            </div>
+                            <div>
+                                <button type="submit" class="btn btn-card-announcement my-1">{{ __('ui.invia') }}</button>
+                            </div>
+                        
+                        </form>
+                    </div>
                 </div>
 
             </div>
-
         </div>
     </div>
 </x-layout>
