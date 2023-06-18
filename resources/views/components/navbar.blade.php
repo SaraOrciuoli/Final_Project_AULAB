83117ac08bcb56c28d733ee25a7b7dbe967ca303
username / AngeloDiNome <angelofulldev@gmail.com>
2023-06-18 20:20:02 +0200

    modifiche estetiche navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,8 +10,8 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto mb-2 mb-lg-0 align-items-center ">
-                    <li class="nav-item ">
+                <ul class="navbar-nav mx-auto justify-content-start d-flex mb-2 mb-lg-0 align-items-md-center border-top-sma">
+                    <li class="nav-item">
                         <a class="nav-link text-acc" aria-current="page"
                             href="{{ route('homepage') }}">{{ __('ui.home') }}</a>
                     </li>
@@ -56,94 +56,91 @@
                     </li>
                 </ul>
 
-
-
-                <div class="d-block d-md-flex">
+                <div class="my-2">
                     {{-- Search button --}}
                     <form method="GET" action="{{ route('search_announcements') }}" class="d-flex box-search">
                         <input type="search" name="searched" class="form-control p-0 form-search text-acc"
                             placeholder="{{ __('ui.ricerca') }}" aria-label="Search">
                             
-                        <button class="btn d-none d-md-block p-0 me-2"
+                        <button class="btn p-0 me-2"
                             type="submit"><i class="fa-solid fa-magnifying-glass fa-lg text-acc"></i></button>
                     </form>
                 </div>
 
-
-
-
                 {{-- Button traduzione --}}
-                
-                <div class="dropdown">
-                    <button class="btn btn-acc dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa-solid fa-earth-europe fa-lg"></i>
-                    </button>
-                    <ul class="dropdown-menu bg-dark drop-translate">
-                        <li><x-_locale lang='it' /></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><x-_locale lang='en' /></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><x-_locale lang='es' /></li>
-                    </ul>
-                </div>
-
-                {{-- dropdown utente --}}
-
-                <ul class="p-0 mb-2 mb-lg-0 align-items-center">
-                    @auth
-                        <li class="nav-item dropdown">
-                            <a class="btn-acc dropdown-toggle text-acc" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fa-solid fa-circle-user fa-lg"></i>
-                            </a>
-                            <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item text-acc" href="#">{{ __('ui.profilo') }}</a></li>
-                                @if (Auth::user()->is_revisor)
+                <div class="d-block d-md-flex">
+                    <div class="dropdown icon-translate">
+                        <a class="btn-acc text-acc dropdown-toggle p-0 my-2 m-md-3 border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-earth-europe fa-lg icon-translate"></i>
+                        </a>
+                        <ul class="dropdown-menu bg-dark w-50 dropdown-menu-sma">
+                            <li><x-_locale lang='it' /></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><x-_locale lang='en' /></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><x-_locale lang='es' /></li>
+                        </ul>
+                    </div>
+    
+                    {{-- dropdown utente --}}
+    
+                    <ul class="p-0 mb-2 mb-lg-0 align-items-center d-flex">
+                        @auth
+                            <li class="nav-item dropdown">
+                                <a class="btn-acc dropdown-toggle text-acc" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa-solid fa-circle-user fa-lg"></i>
+                                </a>
+                                <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item text-acc" href="#">{{ __('ui.profilo') }}</a></li>
+                                    @if (Auth::user()->is_revisor)
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="dropdown-item position-relative text-acc p-revisor" aria-current="page"
+                                                href="{{ route('revisor_index') }}">
+                                                {{ __('ui.zona revisore') }} <span
+                                                    class="position-absolute top-0 end-0 translate-middle badge rounded-pill bg-danger">{{ App\Models\Announcement::toBeRevisionedCount() }}
+                                                    <span class="visually-hidden">messaggio non letto</span>
+                                                </span>
+                                            </a>
+                                        </li>
+                                    @endif
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
-                                    <li class="nav-item">
-                                        <a class="dropdown-item position-relative text-acc p-revisor" aria-current="page"
-                                            href="{{ route('revisor_index') }}">
-                                            {{ __('ui.zona revisore') }} <span
-                                                class="position-absolute top-0 end-0 translate-middle badge rounded-pill bg-danger">{{ App\Models\Announcement::toBeRevisionedCount() }}
-                                                <span class="visually-hidden">messaggio non letto</span>
-                                            </span>
+                                    {{-- Logout --}}
+    
+                                    <li>
+                                        <a class="dropdown-item text-acc" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            {{ __('ui.logout') }}
                                         </a>
+    
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
                                     </li>
-                                @endif
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                {{-- Logout --}}
-
-                                <li>
-                                    <a class="dropdown-item text-acc" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        {{ __('ui.logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </li>
-
-                            </ul>
-                        </li>
-                    @else
-                        <li class="nav-item">
-                            <a href="{{ route('register') }}" class="nav-link text-acc">{{ __('ui.registrati') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('login') }}" class="nav-link text-acc">{{ __('ui.accedi') }}</a>
-                        </li>
-                    @endauth
-                    
-                    
-                </ul>
+    
+                                </ul>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="nav-link text-acc">{{ __('ui.registrati') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('login') }}" class="nav-link text-acc">{{ __('ui.accedi') }}</a>
+                            </li>
+                        @endauth
+                        
+                        
+                    </ul>
+                </div>
+                
 
             </div>
         </div>
