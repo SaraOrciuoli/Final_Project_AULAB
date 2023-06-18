375ab1fdff0de24df7ce7a7ab3585582bf3c134a
username / AngeloDiNome <angelofulldev@gmail.com>
2023-06-18 15:57:09 +0200

    modificata estetica pagina contatti

--- resources/views/mail/contact-us.blade.php
+++ resources/views/mail/contact-us.blade.php
@@ -1,7 +1,26 @@
 <x-layout>
-    <div class="container">
-        <div class="row">
-                @livewire('mail-form')
+    <div class="bg-acc">
+        <div class="container d-flex justify-content-center align-items-center min-vh-100 p-9">
+            <div class="row row-form animation-fade h-100">
+                <div class="col-12 col-md-6 img-side-contact">
+                    <ul class="d-flex justify-content-evenly h-100 flex-column p-0">
+                        <li>
+                            <a href="https://twitter.com/?lang=it" target="_blank" class="box-contact"><i class="fa-brands fa-twitter fa-xl icon-footer"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank" class="box-contact"><i class="fa-brands fa-facebook fa-xl icon-footer"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank" class="box-contact"><i class="fa-brands fa-linkedin fa-xl icon-footer"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://web.whatsapp.com/" target="_blank" class="box-contact"><i class="fa-brands fa-whatsapp fa-xl icon-footer"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                    @livewire('mail-form')
+                </div>
+                
         </div>
     </div>
 </x-layout>
