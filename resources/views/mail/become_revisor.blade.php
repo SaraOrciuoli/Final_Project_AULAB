8d157fb6ea82a438f9f82ee2553a63fb23d31563
Sara Orciuoli <orciuoli.sara@gmail.com>
2023-06-19 18:19:58 +0200

    modifiche pagina email

--- resources/views/mail/become_revisor.blade.php
+++ resources/views/mail/become_revisor.blade.php
@@ -1,5 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mail</title>
+
+
+    @vite(['resources/css/app.css'])
+
+</head>
+
+<body>
+
+    
+    <h1 class="text-acc">Un utente ha richiesto di lavorare con noi</h1>
+    <h2>Ecco i suoi dati:</h2>
+    <p><span class="text-acc">Nome:</span> {{ $user->name }}</p>
+    <p><span class="text-acc">Email:</span> {{ $user->email }}</p>
+    <div class="d-flex">
+        <p>Clicca qui per renderlo revisore </p>
+        <a class=" text-acc ms-2" href="{{ route('make_revisor', compact('user')) }}">Rendi revisore!</a>
+    </div>
+
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- <!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +55,7 @@
 </head>
 <body>
     <div>
-        <h1>Un utente ha richiesto di lavorare con noi</h1>
-        <h2>Ecco i suoi dati:</h2>
-        <p>Nome: {{$user->name}}</p>
-        <p>Email: {{$user->email}}</p>
-        <p>Clicca qui per renderlo revisore</p>
-        <a href="{{route('make_revisor', compact('user'))}}">Rendi revisore!</a>
+        
     </div>
 </body>
-</html>
+</html> --}}
