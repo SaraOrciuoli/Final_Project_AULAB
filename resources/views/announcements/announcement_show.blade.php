d0c55b595f2bdd83e89c14a9653c0b7c6d48d075
username / AngeloDiNome <angelofulldev@gmail.com>
2023-06-19 17:43:41 +0200

    responsive pagina dettaglio

--- resources/views/announcements/announcement_show.blade.php
+++ resources/views/announcements/announcement_show.blade.php
@@ -2,7 +2,7 @@
 
     <div class="container-fluid bg-details">
         <div class="row justify-content-center align-items-center h-100">
-            <div class="col-6 text-center text-white">
+            <div class="col-12 col-md-6 text-center text-white">
                 <h1>{{ __('ui.annuncio') . ' ' . $announcement->title }}</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dignissimos eaque earum aliquam quos?
                     Facilis officia est ad itaque dolore, quos, aut dolores pariatur non ducimus doloremque! Ullam, sit
@@ -12,9 +12,9 @@
     </div>
     
     <div class="bg-acc">
-        <div class="container p-9">
-            <div class="row row-form-detail bg-main animation-fade justify-content-center align-items-center vh-45">
-                <div class="col-7">
+        <div class="container p-4">
+            <div class="row row-form-detail bg-main animation-fade justify-content-center align-items-center h-100 py-5">
+                <div class="col-12 col-md-5 col-lg-7">
                     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             @foreach ($announcement->images as $image)
@@ -38,18 +38,18 @@
                     </div>
                 </div>
                 
-                    <div class="col-5">
-                        <div class="pb-5">
-                            <h5 class="card-title mb-3">Nome: {{ $announcement->title }}</h5>
-                            <p class="card-title">Descrizione: {{ $announcement->description }}</p>
+                    <div class="col-12 col-md-5 col-lg-5">
+                        <div class="pb-0 pb-md-0 pb-lg-5">
+                            <h5 class="card-title my-3 my-md-0">Nome: {{ $announcement->title }}</h5>
+                            <p class="card-title mt-0 mt-md-3">Descrizione: {{ $announcement->description }}</p>
                             <p class="card-subtitle my-3">Prezzo: {{ $announcement->price }}€</p>
                             <a href="{{ route('category_show', ['category' => $announcement->category]) }}"
                                 class="btn btn-card-category">{{ __('ui.categoria') }}: {{ $announcement->category->name }}</a>
                         </div>
                         <div class="pt-5">
-                            <a class="btn btn-card-detail"
+                            <a class="btn btn-card-detail mb-0 mb-md-3 mb-lg-0"
                                 href="{{ route('announcement_edit', compact('announcement')) }}">Modifica Articolo</a>
-                            <a class="btn btn-danger ms-3"
+                            <a class="btn btn-danger"
                                 href="{{ route('announcement_edit', compact('announcement')) }}">Elimina Articolo</a>
                         </div>
                     </div>
